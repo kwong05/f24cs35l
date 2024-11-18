a8ea1f15872a1f6d90662ce2c495cb316d55c03a
Keshiv Tandon <53798357+KeshivT@users.noreply.github.com>
2024-11-17 17:11:37 -0800

    Update Equipment.js

--- Backend/Models/Equipment.js
+++ Backend/Models/Equipment.js
@@ -5,7 +5,7 @@ const User = require('./User');
 const equipmentSchema = new mongoose.Schema({
     equipmentName: {type: String, required: true, unique: true},    // name of the equipment - may or may not be unique
     currentUser: {type: User, required: false},                     // which user is currently using the machine?
-    userQueue: {type: [User], required: true}                       // which users are currently waiting for the machine?
+    userQueue: {type: [User], required: true},                       // which users are currently waiting for the machine?
     unlockTime: {type: Date, required: true, default: Date.now()}   // when the equipment will next be available
 });
 
