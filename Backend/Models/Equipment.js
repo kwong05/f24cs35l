df7372b73f486b4450a1711eabdace076fc6ad3b
Kevin Wong <kevin.g.wong.05@gmail.com>
2024-11-18 11:25:31 -0800

    Equipment: rename for clarity

--- Backend/Models/Equipment.js
+++ Backend/Models/Equipment.js
@@ -3,7 +3,7 @@ const User = require('./User');
 
 // TODO maybe add equipment reservation list to User?
 const equipmentSchema = new mongoose.Schema({
-    equipmentName: {type: String, required: true, unique: true},    // name of the equipment - may or may not be unique
+    name: {type: String, required: true, unique: true},    // name of the equipment - may or may not be unique
     currentUser: {type: User, required: false},                     // which user is currently using the machine?
     userQueue: {type: [User], required: true},                       // which users are currently waiting for the machine?
     unlockTime: {type: Date, required: true, default: Date.now()}   // when the equipment will next be available
