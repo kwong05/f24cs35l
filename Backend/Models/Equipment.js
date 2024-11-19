30b9c09ada0dad46ec772741981572bf566299ca
Keshiv Tandon <53798357+KeshivT@users.noreply.github.com>
2024-11-18 20:57:48 -0800

    Update Equipment.js to use mongoose Id

--- Backend/Models/Equipment.js
+++ Backend/Models/Equipment.js
@@ -3,8 +3,8 @@ const mongoose = require('mongoose');
 // TODO maybe add equipment reservation list to User?
 const equipmentSchema = new mongoose.Schema({
     name: {type: String, required: true, unique: true},    // name of the equipment - may or may not be unique
-    currentUser: {type: String, required: false},                     // which user is currently using the machine?
-    userQueue: {type: [String], required: true},                       // which users are currently waiting for the machine?
+    currentUser: {type: mongoose.Schema.Types.ObjectId, ref: 'User', required: false },                     // which user is currently using the machine?
+    userQueue: {type: [mongoose.Schema.Types.ObjectId], ref: 'User', required: true},                       // which users are currently waiting for the machine?
     unlockTime: {type: Date, required: true, default: Date.now()}   // when the equipment will next be available
 });
 
