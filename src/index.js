19aa605d16ae388bffdd640940323d78089210c5
n-franz <nico.h.franz@gmail.com>
2024-12-02 21:22:29 -0800

    Updated index.js to work with routing

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React, { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./styles.css";
+import { BrowserRouter } from "react-router-dom";
 
 import App from "./App";
 
 const root = createRoot(document.getElementById("root"));
 root.render(
   <StrictMode>
+    BrowserRouter>
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined" />
     <App />
+    </BrowserRouter>
   </StrictMode>
 );
