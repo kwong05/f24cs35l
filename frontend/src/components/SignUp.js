071938a220d45355392f85249f5a67bd31200ef0
ria-kundu <68344120+ria-kundu@users.noreply.github.com>
2024-12-04 13:53:19 -0800

    SignUp.js: removed handleLogin()

--- frontend/src/components/SignUp.js
+++ frontend/src/components/SignUp.js
@@ -27,32 +27,6 @@ function SignUp({ toggle, setMessage, setIsLoggedIn, setUsername }) {
         }
     };
 
-    const handleLogin = async () => {
-        try {
-            const response = await fetch('http://localhost:10000/api/users/login', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify({ username: localUsername, password }),
-            });
-
-            if (response.ok) {
-                setUsername(localUsername);
-                setIsLoggedIn(true);
-                toggle(); // Close the signup popup
-                alert('Registration and login successful!');
-            } else {
-                const errorData = await response.json();
-                toggle();
-                setMessage(errorData.message || 'Login failed. Please check your username and password.');
-            }
-        } catch (error) {
-            toggle();
-            setMessage(error.message);
-        }
-    };
-
     return (
         <div className="popup">
             <div className="popup-inner">
