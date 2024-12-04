421ccc9584be436672f5d504e83c45662f1aaebc
alexandershen27 <alshen@gmail.com>
2024-12-04 13:46:18 -0800

    Import useState in card.js

--- frontend/src/components/Card.js
+++ frontend/src/components/Card.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import CardList from './CardList';
 
 function Card({ machine, joinSeen, toggleJoinPopup, currentPopupId, setMessage, loggedIn, favorite, toggleFavorite }) {
@@ -8,55 +8,55 @@ function Card({ machine, joinSeen, toggleJoinPopup, currentPopupId, setMessage, 
   }
 
   let collapsible_text = machine.userQueue.length + " people waiting..."
-  let estimated_time = machine.unlockTime + " minutes"; 
-    
-  if(machine.userQueue && machine.userQueue.length == 0) {
+  let estimated_time = machine.unlockTime + " minutes";
+
+  if (machine.userQueue && machine.userQueue.length == 0) {
     collapsible_text = "Waitlist is empty"
     estimated_time = "";
   }
-    
-    return (
-        <div className="card">
-            <div className="card-title">
-                {machine.name}
-                <button className="join-waitlist-button" onClick={() => toggleJoinPopup(machine.id)}>
-                    Join
-                </button>
-                {joinSeen && currentPopupId === machine.id && (
-                    <div className="collapsible-est-time">
-                        {machine.estimated_time}
-                    </div>
-                )}
-                {loggedIn ? 
-                  (favorite ? (
-                    <button class="favorites-button" onClick={() => toggleFavorite(machine.id)}>
-                      <span class="material-symbols-outlined active-favorite">favorite</span>
-                  </button>
-                  ) : (
-                    <button class="favorites-button" onClick={() => toggleFavorite(machine.id)}>
-                    <span class="material-symbols-outlined inactive-favorite">favorite</span>
-                </button>
-                  )) : null}
-            </div>
-            <div class="collapsible"> 
-          <button type="button" class="collapsible-button" onClick={() => toggleListOpen()}>
-              <div class="collapsible-description">
-                {machine.userQueue && machine.userQueue.length != 0 ? (
-                  listOpen ? <span class="material-symbols-outlined arrow">keyboard_arrow_down</span> : <span class="material-symbols-outlined arrow">chevron_right</span>
-                ) : null
-                }
-              {collapsible_text}
-              </div>
-              <div class="collapsible-est-time">
-              {estimated_time}
-              </div>
-          </button>
-          <div>
-            {listOpen ? <CardList waitlist={machine.userQueue}/> : null}
+
+  return (
+    <div className="card">
+      <div className="card-title">
+        {machine.name}
+        <button className="join-waitlist-button" onClick={() => toggleJoinPopup(machine.id)}>
+          Join
+        </button>
+        {joinSeen && currentPopupId === machine.id && (
+          <div className="collapsible-est-time">
+            {machine.estimated_time}
           </div>
+        )}
+        {loggedIn ?
+          (favorite ? (
+            <button class="favorites-button" onClick={() => toggleFavorite(machine.id)}>
+              <span class="material-symbols-outlined active-favorite">favorite</span>
+            </button>
+          ) : (
+            <button class="favorites-button" onClick={() => toggleFavorite(machine.id)}>
+              <span class="material-symbols-outlined inactive-favorite">favorite</span>
+            </button>
+          )) : null}
+      </div>
+      <div class="collapsible">
+        <button type="button" class="collapsible-button" onClick={() => toggleListOpen()}>
+          <div class="collapsible-description">
+            {machine.userQueue && machine.userQueue.length != 0 ? (
+              listOpen ? <span class="material-symbols-outlined arrow">keyboard_arrow_down</span> : <span class="material-symbols-outlined arrow">chevron_right</span>
+            ) : null
+            }
+            {collapsible_text}
+          </div>
+          <div class="collapsible-est-time">
+            {estimated_time}
+          </div>
+        </button>
+        <div>
+          {listOpen ? <CardList waitlist={machine.userQueue} /> : null}
         </div>
-        </div>
-    );
+      </div>
+    </div>
+  );
 }
 
 export default Card;
