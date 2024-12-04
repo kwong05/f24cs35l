7757467fedfe76c18bf84799590c39015cc9437f
ria-kundu <68344120+ria-kundu@users.noreply.github.com>
2024-12-04 13:13:45 -0800

    equipmentRoutes.js: moved fetchEquipment here

--- backend/routes/equipmentRoutes.js
+++ backend/routes/equipmentRoutes.js
@@ -8,3 +8,16 @@ router.post('/addEquipment', authenticateToken, isAdmin, addEquipment);
 router.get('/equipment', getEquipment);
 
 module.exports = router;
+
+// get all the inputted equipment from the database to display it in frontend
+app.get('/api/fetchEquipment', async (req, res) => {
+    try {
+        //console.log("DEBUG");
+        const equipmentList = await Equipment.find({}, 'name'); // only get the names
+        //console.log(equimentList);
+        // send names as a json
+        res.json(equipmentList);
+    } catch (err) {
+        res.status(500).json({ message: 'Error retrieving equipment data' });
+    }
+});
