35689f7d8499ad60dc4bc0c02cbc4a6437b2186c
n-franz <nico.h.franz@gmail.com>
2024-12-04 11:07:41 -0800

    Card.js: changed styling

--- frontend/src/components/Card.js
+++ frontend/src/components/Card.js
@@ -4,25 +4,27 @@ import CardList from './CardList';
 function Card({ machine, joinSeen, toggleJoinPopup, currentPopupId, setMessage }) {
     return (
         <div className="card">
-            <h2>{machine.name}</h2>
-            <button onClick={() => toggleJoinPopup(machine.id)}>
-                Join
-            </button>
-            {joinSeen && currentPopupId === machine.id && (
-                <div className="collapsible-est-time">
-                    {machine.estimated_time}
-                </div>
-            )}
-            {loggedIn ? 
-              (favorite ? (
-                <button class="favorites-button" onClick={() => toggleFavorite(machine.id)}>
-                  <span class="material-symbols-outlined active-favorite">favorite</span>
-              </button>
-              ) : (
-                <button class="favorites-button" onClick={() => toggleFavorite(machine.id)}>
-                <span class="material-symbols-outlined inactive-favorite">favorite</span>
-            </button>
-              )) : null}
+            <div className="card-title">
+                {machine.name}
+                <button onClick={() => toggleJoinPopup(machine.id)}>
+                    Join
+                </button>
+                {joinSeen && currentPopupId === machine.id && (
+                    <div className="collapsible-est-time">
+                        {machine.estimated_time}
+                    </div>
+                )}
+                {loggedIn ? 
+                  (favorite ? (
+                    <button class="favorites-button" onClick={() => toggleFavorite(machine.id)}>
+                      <span class="material-symbols-outlined active-favorite">favorite</span>
+                  </button>
+                  ) : (
+                    <button class="favorites-button" onClick={() => toggleFavorite(machine.id)}>
+                    <span class="material-symbols-outlined inactive-favorite">favorite</span>
+                </button>
+                  )) : null}
+            </div>
             <div>
                 <CardList waitlist={machine.waitlist} />
             </div>
