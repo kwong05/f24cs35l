73e382f5e70bdf5352543bf66266885883269027
Keshiv Tandon <53798357+KeshivT@users.noreply.github.com>
2024-12-04 18:30:21 -0800

    Update User.js

--- backend/models/User.js
+++ backend/models/User.js
@@ -5,7 +5,7 @@ const userSchema = new mongoose.Schema({
     password: { type: String, required: true },
     currentEquipment: { type: mongoose.Schema.Types.ObjectId, ref: 'Equipment', required: false },
     queuedEquipment: { type: mongoose.Schema.Types.ObjectId, ref: 'Equipment', required: false },
-    favoritesList: { type: [mongoose.Schema.Types.ObjectId], ref: 'Equipment', required: false }
+    favoritesList: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Equipment', required: false }]
 });
 
 module.exports = mongoose.model('User', userSchema);
