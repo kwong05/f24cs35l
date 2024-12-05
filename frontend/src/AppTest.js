e4fa23e56d14358b3a0db0727aa3a06846cce43d
Keshiv Tandon <53798357+KeshivT@users.noreply.github.com>
2024-12-04 18:59:02 -0800

    Create AppTest.js

---
+++ frontend/src/AppTest.js
@@ -0,0 +1,24 @@
+import './index.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Login from './components/Login';
+import Signup from './components/Signup';
+import AddEquipment from './components/BookView';
+import MainPage from './components/MainPage';
+
+
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/add" element={<AddEquipment />} />
+        <Route path="/main" element={<MainPage />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
