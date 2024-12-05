62cbfcf511c1518699e747646605ccb217d57047
Keshiv Tandon <53798357+KeshivT@users.noreply.github.com>
2024-12-04 18:30:54 -0800

    fixed userQueue to make list

--- backend/models/Equipment.js
+++ backend/models/Equipment.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 const equipmentSchema = new mongoose.Schema({
     name: {type: String, required: true, unique: true},    // name of the equipment - may or may not be unique
     currentUser: {type: mongoose.Schema.Types.ObjectId, ref: 'User', required: false },                     // which user is currently using the machine?
-    userQueue: {type: [mongoose.Schema.Types.ObjectId], ref: 'User', required: false },                       // which users are currently waiting for the machine?
+    userQueue: [{type: mongoose.Schema.Types.ObjectId, ref: 'User', required: false }],                       // which users are currently waiting for the machine?
     unlockTime: {type: Date, required: true, default: Date.now()}   // when the equipment will next be available
 });
 
