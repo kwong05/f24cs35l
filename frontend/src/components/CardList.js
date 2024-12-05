2d0f41f07f2f6651fb3789cc8a0caaf8ec3e56dc
n-franz <nico.h.franz@gmail.com>
2024-12-04 23:15:31 -0800

    CardList.js: display current user above waitlist

--- frontend/src/components/CardList.js
+++ frontend/src/components/CardList.js
@@ -1,24 +1,46 @@
 import React from 'react';
 
-function CardList({ waitlist }) {
+function CardList({ waitlist, machine }) {
+    const [currentUsername, setCurrentUsername] = useState("");
+
     if (!waitlist) {
         return <div></div>;
     }
 
-    const tableRows = waitlist.map((user, index) => (
-        <tr key={index}>
-            <td>{user}</td>
-        </tr>
-    ));
+    useEffect(() => {
+        const fetchUsername = async () => {
+            let curr_id = [machine.currentUser];
+          try {
+            const response = await fetch('http://localhost:10000/api/users/fetchUserDetails', {
+              method: 'POST',
+              headers: {
+                'Content-Type': 'application/json',
+              },
+              body: JSON.stringify({ userIds: curr_id }),
+            });
+            if (!response.ok) {
+              throw new Error('Error retrieving user details');
+            }
+            const data = await response.json();
+            setCurrentUsername(data[0].username);
+          } catch (error) {
+            console.error('Error fetching usernames:', error);
+          }
+        };
+        fetchUsername();
+      }, [machine.userQueue]);
 
     return (
-        <div className="card-list">
-            {waitlist.toReversed().map((username, index) => (
+        <table>
+        <tbody>
+        {currentUsername ? <tr key={0}><td> Current user: {currentUsername} </td></tr> : null}
+        {waitlist.toReversed().map((username, index) => (
                 <div key={index} className="card-list-item">
                     {index+1}. {username}
                 </div>
             ))}
-        </div>
+        </tbody>
+      </table>
     );
 }
 
